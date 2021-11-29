fa8c62ec563a3dea5eb07489037d8cb9c9e434ab
Tsan Aung <myanmar@post.com>
2021-11-29 12:07:38 +0630

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const home_articles = {
+    'blog':'springcondor',
+    'apihome':'https://script.google.com/macros/s/AKfycbzDjBFkxbdNGrrPzGGHm4FJVze3Dtguc9znwt0p7q7VofLAVNL7WLYo01LeptY9tR7Oag/exec'
+};
+
+export{
+    home_articles
+};
