d79bcb91c7d77892d3cd1bed52ac566620ea1f56
Tsan Aung <myanmar@post.com>
2021-12-08 16:21:04 +0630

    Update main_usage.js

--- free-course/tut/main_usage.js
+++ free-course/tut/main_usage.js
@@ -12,5 +12,5 @@ if (data_method === 'article') {
 // ပြင်ရမယ့်နေရာမှာ blog name ထည့်ရမယ်။
 // ဥပမာ let data = 'springcondor'; လို့ ဖြစ်သွားမယ်။
 // Javascript မှာ variable တွေ declare တဲ့အခါ let, const, var စသဖြင့် keyword တွေ သုံးရတယ်။
-// ဒီမှာ let ဆိုတာ JavaScript မှာ variable သတ်မှတ်ဖို့သုံးတဲ့ keyword
-// ဥပမာ let variable_name = 'value'; အဲပုံစံရေးထားတာ
+// ဒီမှာ let ဆိုတာ JavaScript မှာ variable သတ်မှတ်ဖို့သုံးတဲ့ keyword ဥပမာ let variable_name = 'value'; အဲပုံစံရေးထားတာ
+// ခုရေးထားတဲ့စာတွေ (စာကြောင်းရှေးမှာ // ပါတဲ့စာတွေ) မကူးနဲ့၊ အဲဒါ comment ရေးထားတာ
